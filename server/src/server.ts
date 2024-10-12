0e8949e5f9578792d07fc1df837a412a9743a909
unknown <rosa.angel.daniel@gmail.com>
2024-10-12 17:28:00 -0600

    Added todos to server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,25 @@
 import express from 'express';
+// import { ApolloServer } from '@apollo/server';
 import path from 'node:path';
-import db from './config/connection.js';
 import routes from './routes/index.js';
+
+// import {typeDefs, resolvers} from './schemas/index.js';
+import db from './config/connection.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// cont server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// const startApolloServer = async () => {
+//   await server.start();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// app.use('/graphql', server.getMiddleware(server));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -19,3 +31,6 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+// };
+
+// startApolloServer();

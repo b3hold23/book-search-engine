ab9c30b5d5aee1c7b9432d214a7b90b919ff45b3
unknown <rosa.angel.daniel@gmail.com>
2024-10-13 18:30:01 -0600

    added completed typeDefs

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -8,11 +8,26 @@ const typeDefs = `
     }
 
     type Book {
-        bookIs: ID!
-        title: string!
+        _id: ID!
+        title: String!
         authors: [String]
         description: String!
         image: String!
         link: String!
     }
+
+    type Query {
+        user(_id: ID): [User]
+        book(_id: String): [Book]
+    }
+
+    Type Mutation {
+        getSingleUser(id: ID, username: String): User
+        createUser(username: String, email: String, password: String): User
+        login(username: String, email: String, password: String): User
+        saveBook(user: ID, book: ID): User
+        deleteBook(user: ID, book: ID): User
+    }
 `
+;
+export default typeDefs;

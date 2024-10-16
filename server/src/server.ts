a2314fc9d667b2c7c3723ed667c094be0b04302f
unknown <rosa.angel.daniel@gmail.com>
2024-10-16 16:19:05 -0600

    fixing deploy issues

--- server/src/server.ts
+++ server/src/server.ts
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
 
